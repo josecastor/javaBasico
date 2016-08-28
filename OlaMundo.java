083d300d37a01468367ec04badc540ededd90d78
josecastor <junior.castor@gmail.com>
2016-08-28 19:50:02 -0300

    inicio da brincadeira com java

---
+++ OlaMundo.java
@@ -0,0 +1,10 @@
+package br.com.estudo.javabasico;
+
+public class OlaMundo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Olá Mundo");
+	}
+
+}
