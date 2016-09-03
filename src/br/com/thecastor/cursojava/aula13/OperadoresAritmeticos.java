7be58adbec262d14c6cfe7a2c4df32208707bbdd
theCastor <junior.castor@gmail.com>
2016-09-03 11:24:31 -0300

    Operadores

---
+++ src/br/com/thecastor/cursojava/aula13/OperadoresAritmeticos.java
@@ -0,0 +1,44 @@
+package br.com.thecastor.cursojava.aula13;
+
+public class OperadoresAritmeticos {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int resultado = 1 + 2;
+		System.out.println(resultado);
+		
+		resultado = resultado - 1;
+		System.out.println(resultado);
+		
+		resultado = resultado * 2;
+		System.out.println(resultado);
+		
+		resultado = resultado / 2;
+		System.out.println(resultado);
+		
+		resultado = resultado + 8;
+		System.out.println(resultado);
+		
+		resultado = resultado % 7;
+		System.out.println(resultado);
+		
+		String pString = "Hoje é ";
+		String sString = "Sábado";
+		System.out.println(pString + sString);
+		
+		resultado = +1;
+		System.out.println(resultado);
+		
+		resultado--;
+		System.out.println(resultado);
+		
+		resultado++;
+		System.out.println(resultado);
+		
+		resultado = -resultado;
+		System.out.println(resultado);
+
+	}
+
+}
---
+++ src/br/com/thecastor/cursojava/aula13/OperadoresRelacionais.java
@@ -0,0 +1,25 @@
+package br.com.thecastor.cursojava.aula13;
+
+public class OperadoresRelacionais {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int valor1 = 1;
+		int valor2 = 2;
+		
+		System.out.println("valor1 == valor2 -> " + (valor1 == valor2));
+		System.out.println("valor1 != valor2 -> " + (valor1 != valor2));
+		System.out.println("valor1 > valor2 -> " + (valor1 > valor2));
+		System.out.println("valor1 >= valor2 -> " + (valor1 >= valor2));
+		System.out.println("valor1 < valor2 -> " + (valor1 < valor2));
+		System.out.println("valor1 <= valor2 -> " + (valor1 <= valor2));
+		/*
+		boolean sucess = false;
+		System.out.println(sucess);
+		System.out.println(!sucess);
+		*/
+		
+	}
+
+}
